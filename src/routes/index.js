b2f50ed957765c2989e1843abac499b9620ccbb0
Roberto Sousa <robertosousa186@gmail.com>
2022-05-11 22:25:23 -0300

    using route 'pessoas'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
+const pessoas = require('./pessoasRoute');
 
 module.exports = (app) => {
   app.use(express.json());
-
-  app.get('/', (req, res) => {
-    res.send('olá');
-  });
+  app.use(pessoas);
 };
