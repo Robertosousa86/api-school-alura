19d8712769d7b7e4378723f94b9ce2a4f84ed2a0
Roberto Sousa <robertosousa186@gmail.com>
2022-05-11 22:58:44 -0300

    Using method in the route

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.getPessoas);
+router.get('/pessoas/:id', PessoaController.getPessoasById);
 
 module.exports = router;
