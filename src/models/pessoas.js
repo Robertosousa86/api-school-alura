cc087d0420111433f4017da400b6dfa63ec66bb7
Roberto Sousa <robertosousa186@gmail.com>
2022-05-14 22:20:33 -0300

    Creating associations

--- src/models/pessoas.js
+++ src/models/pessoas.js
@@ -1,26 +1,28 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
+      Pessoas.hasMany(models.Turmas, {
+        foreignKey: 'docente_id',
+      });
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: 'estudantes_id',
+      });
     }
   }
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoas',
-  });
+  Pessoas.init(
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Pessoas',
+    }
+  );
   return Pessoas;
 };
