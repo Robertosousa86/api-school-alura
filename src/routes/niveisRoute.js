a16cd0ef8669f5d2c72283a952cf7871cec6b8de
Roberto Sousa <robertosousa186@gmail.com>
2022-05-18 22:02:05 -0300

    Creating another routes

--- src/routes/niveisRoute.js
+++ src/routes/niveisRoute.js
@@ -4,5 +4,12 @@ const NivelController = require('../controllers/NivelController');
 const router = Router();
 
 router.get('/niveis', NivelController.getNiveis);
+router.get('/niveis/:id', NivelController.getNivelById);
+
+router.post('/niveis', NivelController.createNivel);
+
+router.put('/niveis/:id', NivelController.updateNivel);
+
+router.delete('/niveis/:id', NivelController.deleteNivel);
 
 module.exports = router;
